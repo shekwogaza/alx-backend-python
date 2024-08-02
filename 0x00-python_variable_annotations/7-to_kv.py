2a3b1e77a27df38159cda8d1e66a8521836c57a7
GEEGII <gazasolomon@gmail.com>
2024-08-02 01:43:43 +0100

    update on requirements

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to create a tuple from a string and a number.
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Create a tuple from a string and the square of a number.
+
+    Args:
+        k (str): The string to be used as the first element of the tuple.
+        v (Union[int, float]): The number to be squared and used as the second element of the tuple.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the input string and the square of the input number.
+    """
+    return (k, float(v ** 2))
