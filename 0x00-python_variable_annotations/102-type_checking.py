3f26e7c1fc0d6af1bda57d0c546a2a00af637b1c
GEEGII <gazasolomon@gmail.com>
2024-08-02 01:57:21 +0100

    update

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,7 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to safely get
+a value from a dictionary.
+"""
+
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Create a zoomed-in version of the input tuple by repeating each
+    element a given number of times.
+
+    Args:
+        lst (Tuple): The input tuple to be zoomed in.
+        factor (int): The number of times each element in the tuple should
+        be repeated. Default is 2.
+
+    Returns:
+        List: A list containing the elements of the input tuple
+        repeated by the given factor.
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
