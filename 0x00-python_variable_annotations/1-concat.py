18859fe6c5fc79689e9261fb65b1ea8f429d443d
GEEGII <gazasolomon@gmail.com>
2024-08-02 01:34:10 +0100

    update on compliance

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@
 This module provides a function for string concatenation with type annotations.
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenates two strings and returns the result.
