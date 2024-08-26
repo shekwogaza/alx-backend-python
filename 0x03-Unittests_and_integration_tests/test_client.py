f9d95ce269da639e7f577b7ee39879ee3784d4c9
GEEGII <gazasolomon@gmail.com>
2024-08-26 23:35:59 +0100

    update 5

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Unit tests for client.GithubOrgClient class."""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """_summary_
+
+    Args:
+        unittest (_type_): _description_
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """_summary_
+
+        Args:
+            org_name (_type_): _description_
+            mock_get_json (_type_): _description_
+        """
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+
+
+if __name__ == '__main__':
+    unittest.main()
